51922757cbb4aa18e3b4354ee25c378e2a744e40
SamuPr. <provaroni.samuele@itisgrassi.edu.it>
2025-03-22 11:56:41 +0100

    Add files via upload

--- bottiglia.py
+++ bottiglia.py
@@ -1,34 +1,36 @@
-class Bottiglia: #class x creare la classe bottiglia
-    def __init__(self, capacita): #init istanzazione di un oggetto
-        self.capacita = capacita
-        self.quantita = 0
-    def __str__(self):
-        #return str(self.quantita)+"/"+str(self.capacita)
-        #return"{}/{}".format(self.quantita,self.capacita)
-        return f"{self.quantita}/{self.capacita}"
+class Bottiglia:
+    def __init__(self, capacità):                               #il costruttore (__init__) è il metodo che serve per creare un oggetto #"self" è un parametro obbligatorio per i metodi dell'oggetto
+        self.quantità = 0                                       #valore fisso
+        self.capacità = capacità                                #valore passato
     
-    def riempi(self, q):
-        self.quantita += q
-        if self.quantita > self.capacita:
-            self.quantita = self.capacita
-        
-    def svuota(self, q):
-        self.quantita -= q
-        if self.quantita < 0:
-            self.quantita = 0
+    def __str__(self):                                          #definisce come far diventare l'oggetto una stringa
+        return str(self.quantità)+"/"+str(self.capacità) 
+        return "{}/{}",format(self.quantità,self.capacità)
+        return f"{self.quantità}/{self.capacità}"
+    
+    def riempi(self,q):
+        self.quantità += q
+        if self.quantità > self.capacità:                       #fa in modo che anche riempendo di più, ti dia un massimo di 500
+            self.quantità = self.capacità
 
-class BottigliaconTappo(Bottiglia): #eredita sottoclasse bottiglia
-    def __init__(self, capacita):
-        super().__init__(capacita) #super chiama la funzione costruttore di bottiglia
+    def svuota(self,q):
+        self.quantità -= q
+        if self.quantità < 0:                                   #fa in modo che anche svuotando di meno, non ti dia meno di 0
+            self.quantità = 0
+
+class BottigliaConTappo(Bottiglia):                             #Abbiamo creato una sottoclasse che sia una copia della classe madre
+    def __init__(self, capacità):
+        super().__init__(capacità)                              #"super" serve a chiamare la classe madre
         self.aperta = True
     def apri(self):
         self.aperta = True
-        
+        print("La bottiglia è aperta")
     def chiudi(self):
         self.aperta = False
+        print("La bottiglia è chiusa")
     def __str__(self):
         return super().__str__() + f" aperta: {self.aperta}"
-    
-    def riempi(self, q):
-        if self.aperta:
+    def riempi(self, q):                                        #la funziona doveva essere riscritta per non andare in overide
+        if self.aperta: 
             super().riempi(q)
+    
